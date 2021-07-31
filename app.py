030f83bc8b87ee1d0be94071da233b02c3ccf903
gmiesner <ella.miesner@gmail.com>
2021-07-31 17:22:56 -0500

    3rd viz

--- app.py
+++ app.py
@@ -27,6 +27,8 @@ total_budget = []
 total_income = []
 domestic = []
 colors = []
+avg_domestic = []
+domestic_inflation = []
 inflation_avg_budget =[]
 for i in data['year']: 
     if i not in years:
@@ -55,7 +57,6 @@ for k in data['budget']:
   index_budget = years.index(element_budget, 0, len(years))
   total_budget[index_budget] += k
 
-print(data['domgross'])
 count_domestic = -1
 for nine in data['domgross']:
     count_domestic += 1 
@@ -69,11 +70,15 @@ years.reverse()
 years_count.reverse()
 total_income.reverse()
 domestic.reverse()
-print(domestic)
 count_avg_budget = -1
 for y in total_budget: 
     count_avg_budget += 1 
     avg_budget.append(y/ years_count[count_avg_budget])
+
+count_avg_domestic = -1
+for o in domestic: 
+    count_avg_domestic += 1
+    avg_domestic.append(o/ years_count[count_avg_domestic])
 
 for color in years:
     if color > 1969 and color < 1980: 
@@ -104,6 +109,12 @@ for cost in avg_budget:
     t = float(cpi['cpi'][cpi_2020] / cpi['cpi'][count_cpi])
     inflation_avg_budget.append(int(round(cost * t, 0)))
 
+count_cpi_domestic = -1
+for u in avg_domestic: 
+    count_cpi_domestic +=1 
+    i9 = float(cpi['cpi'][cpi_2020] / cpi['cpi'][count_cpi_domestic])
+    domestic_inflation.append(int(round(i9*u, 0)))
+
 # print(inflation_avg_budget[0])
 # avg_budget = [int(round(dummy, 0)) for dummy in avg_budget]
 
@@ -119,12 +130,20 @@ df2 = pd.DataFrame({
     " ": colors
 })
 
+df3 = pd.DataFrame({
+    "Average Domestic Grossing (in USD), adjusted for inflation": domestic_inflation, 
+    "Percent Passed": percent_passed,
+    " ": colors
+})
+
 fig = px.line(df, x="Year", y="Percent Passed", title = "Percent of Movies that Passed the Bechdel Test by Year")
 fig.update_traces(line_color='pink', hovertemplate = '<b>%{x}</b> <br> Percent Passed: %{y}%')
 
 fig2 = px.scatter(df2, x = "Average Budget (in USD), adjusted for inflation", y = "Percent Passed", title = "How Much Hollywood Values Women", color = " ")
 fig2.update_traces(hovertemplate = 'Percent Passed: %{y}% <br> Average Budget: $%{x}')
 
+fig3 = px.scatter(df3, x = "Average Domestic Grossing (in USD), adjusted for inflation", y = "Percent Passed", title = "How Much Society Values Women", color = " ")
+fig3.update_traces(hovertemplate = 'Percent Passed: %{y}% <br> Average Grossing: $%{x}')
 app.layout = html.Div(children=[
     html.H1(children='Bechdel Test', id = "title", style = {'fontStyle': 'italic', 'fontWeight':'800'}),
 
@@ -148,10 +167,12 @@ app.layout = html.Div(children=[
         figure = fig2, 
     ), 
 
-    # dcc.Graph(
-    #     id = 'example-graph3', 
-    #     figure = fig3
-    # )
+    dcc.Graph(
+        id = 'example-graph3', 
+        figure = fig3
+    ), 
+    html.Div(children = '''
+        ** The data is not completely accurate, as the data set we used was missing the domestic grossing for 57 films, which lowered the average. **     ''')
 ], style = {'color':'black', 'textAlign':'center'})
 
 if __name__ == '__main__':
